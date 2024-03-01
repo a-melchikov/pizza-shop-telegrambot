54fd3fc907cf767f85af154bc947f20c2ebd5c0c
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-03-01 10:41:58 +0400

    Added a menu to the left of typing messages.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from handlers.user_private import user_private_router
+from common.bot_commands_list import private
 import asyncio
 import os
 
@@ -19,6 +20,7 @@ dp.include_router(user_private_router)
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
+    await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
     await dp.start_polling(bot, allow_updates=ALLOWED_UPDATES)
 
 asyncio.run(main())
