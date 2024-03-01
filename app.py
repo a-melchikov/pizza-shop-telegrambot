c4187ff95ea52cb30ae359f888456e3099a97dc7
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-03-01 10:17:01 +0400

    Added the handlers file where routers are stored. The router user_private_router has also been added

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from handlers.user_private import user_private_router
 import asyncio
 import os
 
@@ -5,22 +6,19 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 
 from dotenv import find_dotenv, load_dotenv
-
 load_dotenv(find_dotenv())
+
+
+ALLOWED_UPDATES = ['message, edited message']
 
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher()
 
-@dp.message(CommandStart())
-async def start_cmd(message: types.Message):
-    await message.answer('Это была команда старт')
-    
-@dp.message()
-async def start_cmd(message: types.Message):
-    await message.answer(message.text)
+dp.include_router(user_private_router)
+
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
-    
+    await dp.start_polling(bot, allow_updates=ALLOWED_UPDATES)
+
 asyncio.run(main())
