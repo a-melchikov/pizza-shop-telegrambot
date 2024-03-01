81b3c613390988d0a50e83e4e3b5e261692e7d35
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-03-01 10:25:39 +0400

    Three commands about, payment, and shipping have been added.

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -12,3 +12,18 @@ async def start_cmd(message: types.Message):
 @user_private_router.message(Command('menu'))
 async def menu_cmd(message: types.Message):
     await message.answer("Вот меню:")
+
+
+@user_private_router.message(Command('about'))
+async def about_cmd(message: types.Message):
+    await message.answer("О нас:")
+
+
+@user_private_router.message(Command('payment'))
+async def payment_cmd(message: types.Message):
+    await message.answer("Варианты оплаты")
+
+
+@user_private_router.message(Command('shipping'))
+async def shipping_cmd(message: types.Message):
+    await message.answer("Варианты доставки:")
