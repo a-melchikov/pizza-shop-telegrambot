d761b9fcc2a7d50208f221ce76479cb74844f184
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-03-01 10:59:44 +0400

    Magic filters for commands have been added.

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,5 +1,5 @@
-from aiogram import types, Router
-from aiogram.filters import CommandStart, Command
+from aiogram import F, types, Router
+from aiogram.filters import CommandStart, Command, or_f
 
 user_private_router = Router()
 
@@ -9,21 +9,24 @@ async def start_cmd(message: types.Message):
     await message.answer("Привет, я виртуальный помощник")
 
 
-@user_private_router.message(Command('menu'))
+@user_private_router.message(or_f(Command('menu'), (F.text.lower() == "меню")))
 async def menu_cmd(message: types.Message):
     await message.answer("Вот меню:")
 
 
+@user_private_router.message(F.text.lower() == "о нас")
 @user_private_router.message(Command('about'))
 async def about_cmd(message: types.Message):
     await message.answer("О нас:")
 
 
+@user_private_router.message(F.text.lower() == "варианты оплаты")
 @user_private_router.message(Command('payment'))
 async def payment_cmd(message: types.Message):
     await message.answer("Варианты оплаты")
 
 
+@user_private_router.message(F.text.lower().contains('доставк'))
 @user_private_router.message(Command('shipping'))
 async def shipping_cmd(message: types.Message):
     await message.answer("Варианты доставки:")
