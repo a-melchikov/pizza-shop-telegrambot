78f29b94bd2b552a9000e283537337be1f821f27
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-03-05 16:30:00 +0400

    Transferring sqlalchemy ORM sessions via the middleware layer to the handler.

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
 
 from common.bot_cmds_list import private
-from database.engine import create_db, drop_db
+from database.engine import create_db, drop_db, session_maker
 from handlers.admin_private import admin_router
 from handlers.user_group import user_group_router
 from handlers.user_private import user_private_router
+from middlewares.db import DataBaseSession
 
 from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
@@ -40,6 +41,8 @@ async def on_shutdown(bot):
 async def main():
     dp.startup.register(on_startup)
     dp.shutdown.register(on_shutdown)
+
+    dp.update.middleware(DataBaseSession(session_poll=session_maker))
 
     await bot.delete_webhook(drop_pending_updates=True)
     # await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats())
