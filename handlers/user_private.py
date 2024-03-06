3bed42c6930e3d13a3647e9c20dd2eacaec8e974
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-03-06 21:10:17 +0400

    Added functions for receiving goods, editing and deleting. The menu is also implemented.

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,3 +1,4 @@
+from sqlalchemy.ext.asyncio import AsyncSession
 from aiogram import F, types, Router
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command, or_f
@@ -5,7 +6,8 @@ from aiogram.utils.formatting import (
     as_list,
     as_marked_section,
     Bold,
-)  # Italic, as_numbered_list и тд
+)
+from database.orm_query import orm_get_products  # Italic, as_numbered_list и тд
 
 from filters.chat_types import ChatTypeFilter
 
@@ -32,7 +34,13 @@ async def start_cmd(message: types.Message):
 
 # @user_private_router.message(F.text.lower() == "меню")
 @user_private_router.message(or_f(Command("menu"), (F.text.lower() == "меню")))
-async def menu_cmd(message: types.Message):
+async def menu_cmd(message: types.Message, session: AsyncSession):
+    for product in await orm_get_products(session):
+        await message.answer_photo(
+            product.image,
+            caption=f"<strong>{product.name}\
+                </strong>\n{product.description}\nСтоимость: {round(product.price, 2)}",
+        )
     await message.answer("Вот меню:")
 
 
