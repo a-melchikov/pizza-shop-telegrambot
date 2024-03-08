ad45d7aa5616f787395fee3ee793c5458cb91cbd
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-03-08 23:38:17 +0400

    New database tables have been added, including product categories. Functions with the necessary requests to the ORM have been prepared In the admin's FSM and the commands for viewing/adding/changing, respectively, the category item also appeared When the bot starts, the tables are automatically filled in: category and banner Added a "micro" FSM for the admin, to add an image to display background information about the store, like about, payment options, delivery, etc. The user's commands have been removed, as they will not be needed in the future. To remove the menu button with them in your bot, uncomment this line in the app.py -> main: The old database schema is not suitable for this bot, so when you run this bot in !the first time is to delete the old tables (this is already provided, you need to uncomment on !first run this line of code in the file app.py:

--- app.py
+++ app.py
@@ -4,19 +4,22 @@ import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
 
-from common.bot_cmds_list import private
-from database.engine import create_db, drop_db, session_maker
-from handlers.admin_private import admin_router
-from handlers.user_group import user_group_router
-from handlers.user_private import user_private_router
-from middlewares.db import DataBaseSession
-
 from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
 
+from middlewares.db import DataBaseSession
 
-# ALLOWED_UPDATES = ['message, edited_message', 'callback_query']
+from database.engine import create_db, drop_db, session_maker
+
+from handlers.user_private import user_private_router
+from handlers.user_group import user_group_router
+from handlers.admin_private import admin_router
+
+# from common.bot_cmds_list import private
+
+
+# ALLOWED_UPDATES = ['message', 'edited_message', 'callback_query']
 
 bot = Bot(token=os.getenv("TOKEN"), parse_mode=ParseMode.HTML)
 bot.my_admins_list = []
@@ -29,9 +32,9 @@ dp.include_router(admin_router)
 
 
 async def on_startup(bot):
-    run_param = False
-    if run_param:
-        await drop_db()
+
+    # await drop_db()
+
     await create_db()
 
 
@@ -47,9 +50,7 @@ async def main():
 
     await bot.delete_webhook(drop_pending_updates=True)
     # await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats())
-    await bot.set_my_commands(
-        commands=private, scope=types.BotCommandScopeAllPrivateChats()
-    )
+    # await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 
--- database/engine.py
+++ database/engine.py
@@ -2,7 +2,9 @@ import os
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from database.models import Base
+from database.orm_query import orm_add_banner_description, orm_create_categories
 
+from common.texts_for_db import categories, description_for_info_pages
 
 # from .env file:
 # DB_LITE=sqlite+aiosqlite:///my_base.db
@@ -10,7 +12,7 @@ from database.models import Base
 
 engine = create_async_engine(os.getenv("DB_LITE"), echo=True)
 
-# engine = create_async_engine(os.getenv("DB_URL"), echo=True)
+# engine = create_async_engine(os.getenv('DB_URL'), echo=True)
 
 session_maker = async_sessionmaker(
     bind=engine, class_=AsyncSession, expire_on_commit=False
@@ -20,6 +22,10 @@ session_maker = async_sessionmaker(
 async def create_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
+    async with session_maker() as session:
+        await orm_create_categories(session, categories)
+        await orm_add_banner_description(session, description_for_info_pages)
 
 
 async def drop_db():
