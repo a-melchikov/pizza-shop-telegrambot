076290f0c86e3341407cd8a5b3a6e50f3396b975
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-03-10 23:36:59 +0400

    It says adding an item to the cart, the buy button.

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -27,10 +27,25 @@ async def start_cmd(message: types.Message, session: AsyncSession):
     )
 
 
+async def add_to_cart(callback: types.CallbackQuery, callback_data: MenuCallBack, session: AsyncSession):
+    user = callback.from_user
+    await orm_add_user(
+        session,
+        user_id=user.id,
+        first_name=user.first_name,
+        last_name=user.last_name,
+        phone=None,
+    )
+    await orm_add_to_cart(session, user_id=user.id, product_id=callback_data.product_id)
+    await callback.answer("Товар добавлен в корзину.")
+
+
 @user_private_router.callback_query(MenuCallBack.filter())
-async def user_menu(
-    callback: types.CallbackQuery, callback_data: MenuCallBack, session: AsyncSession
-):
+async def user_menu(callback: types.CallbackQuery, callback_data: MenuCallBack, session: AsyncSession):
+
+    if callback_data.menu_name == "add_to_cart":
+        await add_to_cart(callback, callback_data, session)
+        return
 
     media, reply_markup = await get_menu_content(
         session,
